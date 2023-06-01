9c354f4722c24cd32abed86c41855727e9137020
MykolaMailoKPI <134944476+MykolaMailoKPI@users.noreply.github.com>
2023-06-01 10:11:17 +0300

    Update Layout.js

--- practices/src/components/Layout/Layout.js
+++ practices/src/components/Layout/Layout.js
@@ -1,1 +1,22 @@
+import { Outlet, Link } from "react-router-dom";
 
+export const Layout = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Практична робота 1</Link>
+                    </li>
+                    <li>
+                        <Link to="/second">Практична робота 2</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+        </>
+    )
+};
+
+export default Layout;
