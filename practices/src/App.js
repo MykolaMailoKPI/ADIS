fb7418c06d46c03fb7fb1c522d103c0ca7d81249
MykolaMailoKPI <134944476+MykolaMailoKPI@users.noreply.github.com>
2023-06-01 10:27:03 +0300

    Update App.js

--- practices/src/App.js
+++ practices/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import { Reimplement } from './components/Reimplement';
+import { Second } from './components/Second';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Layout } from './components/Layout'
 
 function App() {
   return (
     <div className="App">
-        <Reimplement/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Reimplement />} />
+                    <Route path="second" element={<Second />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
